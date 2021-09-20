b247ed7ff9e932f7143d65f11ab8c44600ae1e5d
afik <afikziv@gmail.com>
2021-09-20 15:08:14 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import Footer from "./components/UI/Footer";
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
